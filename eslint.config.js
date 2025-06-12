2a5c651752457a2c946a8f768ea2f44aa3f4d799
Gabriel Fernando <gf313074@gmail.com>
2025-06-12 11:42:26 -0300

    create initial structure

---
+++ eslint.config.js
@@ -0,0 +1,35 @@
+import globals from 'globals';
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+
+export default tseslint.config(
+  { ignores: ['dist', 'node_modules'] },
+
+  {
+    extends: [
+      eslint.configs.recommended,
+      tseslint.configs.strictTypeChecked,
+      eslintConfigPrettier,
+    ],
+    files: ['src/**/*.{ts}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: globals.node,
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+      '@typescript-eslint/no-unused-vars': 'error',
+    },
+  },
+);
---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import fastify from 'fastify';
+
+const app = fastify();
+
+app.get('/hello', () => {
+  return 'hello world';
+});
+
+app
+  .listen({ port: 3333 })
+  .then(() => {
+    console.log('HTTP server running');
+  })
+  .catch((error: unknown) => {
+    console.log(error);
+  });
