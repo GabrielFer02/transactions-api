9be284a3908625b70b0487c3cc9b7d436e5d3b0b
Gabriel Fernando <gf313074@gmail.com>
2025-06-17 17:22:45 -0300

    create transactions listen

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,16 +1,36 @@
-import { type FastifyInstance } from 'fastify';
+import { FastifyInstance } from 'fastify';
 import zod from 'zod';
 import { randomUUID } from 'node:crypto';
 import { knex } from '../database.js';
 
 export async function transactionsRoutes(app: FastifyInstance) {
+  app.get('/', async () => {
+    const transactions = await knex('transactions').select();
+
+    return { transactions };
+  });
+
+  app.get('/:id', async request => {
+    const getTransactionsParamsSchema = zod.object({
+      id: zod.string().uuid(),
+    });
+
+    const { id } = getTransactionsParamsSchema.parse(request.params);
+
+    const transaction = await knex('transactions').where('id', id).first();
+
+    return {
+      transaction,
+    };
+  });
+
   app.post('/', async (request, reply) => {
     const createTransactionBodySchema = zod.object({
       title: zod.string(),
       amount: zod.number(),
       type: zod.enum(['credit', 'debit']),
     });
- 
+
     const { title, amount, type } = createTransactionBodySchema.parse(
       request.body,
     );
